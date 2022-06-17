c88389636326e1fe66669f39cfb487e02122066d
EAncoHuaracha <eancohu@unsa.edu.pe>
2022-06-16 21:03:55 -0500

    se creo la funcion agregar de manera que se agrega al final

--- EjercicioExtra/linked_list.c
+++ EjercicioExtra/linked_list.c
@@ -9,6 +9,9 @@ typedef struct node
 }
 node;
 
+//declaramos la funcion agregar
+node *agregar(node *l, int num);
+
 int main(){
 	node *list=NULL;
 
@@ -18,14 +21,15 @@ int main(){
 	//pedimos ingresar la cantidad de n numeros para la lista
 	printf("ingrese la cantidad de numeros: ");
 	scanf("%d", &n);
-	
+
 	//Bucle para preguntar las n veces
 	for(int i=0; i<n; i++){
 
 		//pedir numero
 		printf("Ingrese el numero: ");
 		scanf("%d", &num);
-		printf("%d\n", num);
+		//se llama a la funcion agregar
+		list = agregar(list, num);
 
 	}
 
@@ -38,7 +42,7 @@ int main(){
 
 	}
 
-	// función (free) BUENA PR´ACTICA EN PROGRAMACI´ON EN "C"
+	// función (free) BUENA PRÁCTICA EN PROGRAMACIÓN EN "C"
 	while(list!=NULL){
 		node *tmp=list->next;
 		free(list);
@@ -48,3 +52,26 @@ int main(){
 	return 0;
 
 }
+
+node *agregar(node *l, int num){
+	//asiganmos memoria para node
+	node *new = malloc(sizeof(node));
+	node *aux;
+	if(new!=NULL){
+		// se da el valor del num ingresado
+		new->number=num;
+		new->next=NULL;
+
+		if(l==NULL)
+			l=new;
+		else{
+			aux = l;
+			while(aux->next!=NULL)
+
+				aux = aux->next;
+			aux->next=new;
+		}
+	}
+	return l;
+}
+
